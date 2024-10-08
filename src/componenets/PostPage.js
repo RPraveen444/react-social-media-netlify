126febc05e3b3fad1cbe32be30d2a7cdc47b9bc5
RPraveen444 <113783009+RPraveen444@users.noreply.github.com>
2024-10-08 22:07:54 +0530

    Update PostPage.js

--- src/componenets/PostPage.js
+++ src/componenets/PostPage.js
@@ -1,4 +1,4 @@
-import { useParams, Link, useHistory } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useContext } from 'react';
 import DataContext from '../context/DataContext';
 
